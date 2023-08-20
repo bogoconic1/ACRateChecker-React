c73ecd3374e97cfd554d40f1d00a0cbdc74955da
bogoconic1 <tqy8587@gmail.com>
2023-08-20 14:10:34 +0800

    added: verdict classifier

--- src/backend/main.py
+++ src/backend/main.py
@@ -86,4 +86,84 @@ async def get_contest_snapshots(contestName: str):
                             "ac_percent": f'{acceptance_rate}'})
     
     print(dur_contest)
-    return dur_contest
+    return {"dur_contest": dur_contest, "prog_langs": ["All Languages"] + sorted(list(set(cf_dataframe.programmingLanguage)))}
+
+@app.get("/get_verdicts")
+async def get_verdicts(contestName: str, selected_problem: str, selected_language: str):
+    
+    if contestName not in name2id:
+        return "Invalid contest name"
+    contest = name2id[contestName]
+    
+    page = "https://codeforces.com/api/contest.status?contestId=" + str(contest) + "&from=1"
+    cf_submissions_api = requests.get(page)
+    submissions = cf_submissions_api.json()
+    cf_dataframe = pd.json_normalize(submissions,['result'])
+    
+    cf_duration_api = requests.get("https://codeforces.com/api/contest.list?gym=false")
+    contests = cf_duration_api.json()
+    cf_duration_dataframe = pd.json_normalize(contests,['result'])
+    
+    selected_contest = cf_duration_dataframe.loc[cf_duration_dataframe["id"] == int(contest)]
+    start_time = list(selected_contest["startTimeSeconds"])[0]
+    duration = list(selected_contest["durationSeconds"])[0]
+    
+    #minutes_after_start = int(input())
+    #seconds_after_start = minutes_after_start * 60
+    
+    end_time = list(selected_contest["startTimeSeconds"])[0] + duration #seconds_after_start
+    
+    compressed_df = None
+    selected_problem = selected_problem.split("-")[0].rstrip(" ")
+    print(selected_problem)
+    print(selected_language)
+    if selected_language == "All Languages":
+        compressed_df = cf_dataframe[
+            (cf_dataframe["problem.index"] == selected_problem)
+            & (cf_dataframe["creationTimeSeconds"] <= end_time)
+        ]
+    else:
+        compressed_df = cf_dataframe[
+            (cf_dataframe.programmingLanguage == selected_language)
+            & (cf_dataframe["problem.index"] == selected_problem)
+            & (cf_dataframe["creationTimeSeconds"] <= end_time)
+        ]
+    compressed_df["temp"] = compressed_df["verdict"].apply(
+        lambda x: x.capitalize().replace("_", " ").replace("Ok", "Accepted")
+    )
+    compressed_df["passedTestCount"] += 1
+    compressed_df["passedTestCount"] = compressed_df["passedTestCount"].astype(str)
+    verdicts_with_test_no = [
+        "Wrong answer",
+        "Time limit exceeded",
+        "Memory limit exceeded",
+        "Idleness limit exceeded",
+        "Runtime error",
+    ]
+    conditions = [
+        compressed_df["temp"] == "Wrong answer",
+        compressed_df["temp"] == "Time limit exceeded",
+        compressed_df["temp"] == "Memory limit exceeded",
+        compressed_df["temp"] == "Idleness limit exceeded",
+        compressed_df["temp"] == "Runtime error",
+    ]
+    choices = [
+        compressed_df["temp"] + " on test " + compressed_df["passedTestCount"],
+        compressed_df["temp"] + " on test " + compressed_df["passedTestCount"],
+        compressed_df["temp"] + " on test " + compressed_df["passedTestCount"],
+        compressed_df["temp"] + " on test " + compressed_df["passedTestCount"],
+        compressed_df["temp"] + " on test " + compressed_df["passedTestCount"],
+    ]
+    compressed_df["final_verdict"] = np.select(
+        conditions, choices, default=compressed_df["temp"]
+    )
+
+    v_counts = compressed_df["final_verdict"].value_counts()
+    verdict = []
+    for x in v_counts.keys():
+        verdict.append({"verdict": x, 
+                        "submissions": str(v_counts[x])})
+        
+    print(verdict)
+    return verdict
+    
